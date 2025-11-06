51a1614c195384d06634fc547c34de3663b3b775
Yagmur <yagmur@Selmans-MacBook-Air.local>
2025-11-06 12:19:36 +0300

    jenkins trigger

--- dotnet-core-hello-world-web-app/Program.cs
+++ dotnet-core-hello-world-web-app/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! My name is Yagmur Ulas Yucel.");
+app.MapGet("/", () => "Hello World! My name is Yagmur Ulas Yucel. This is my .net web app.");
 
 app.Run();
