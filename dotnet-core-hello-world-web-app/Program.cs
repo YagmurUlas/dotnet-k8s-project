b270ce84d4954e1cc1319e2380922822a2e7438b
Yagmur <yagmur@Selmans-MacBook-Air.local>
2025-11-06 12:15:18 +0300

    jenkins file updated polling added

--- dotnet-core-hello-world-web-app/Program.cs
+++ dotnet-core-hello-world-web-app/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! My name is Yagmur Ulas.");
+app.MapGet("/", () => "Hello World! My name is Yagmur Ulas Yucel.");
 
 app.Run();
